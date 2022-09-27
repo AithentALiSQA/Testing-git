fc32a98324a9d47618d8f19a9279ac874ab50cf6
sshukla <sshukla@AithGGN1147.del.aithent.com>
2022-09-27 11:27:02 +0530

    TestSecondcomit

---
+++ TestGit/src/gitPackage/TestClassGit.java
@@ -0,0 +1,12 @@
+package gitPackage;
+
+public class TestClassGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello git");
+		System.out.println("New text");
+
+	}
+
+}
